64a1faf529109376d30a3320a56a7c18f7f5aaa6
Koki Wataoka <contact@wataoka.com>
2018-02-14 16:22:33 +0900

    Initial Commit

---
+++ upCamera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  upCamera
+//
+//  Created by wataokakoki on 2018/02/14.
+//  Copyright © 2018年 wataokakoki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
